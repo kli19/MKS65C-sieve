d4b0a41bf9aafcc9948e0c177a9a255fefbbbb18
kli19 <kli19@stuy.edu>
2018-10-19 15:23:24 -0400

    allocate size of array

---
+++ sieve.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+int * generateArray(int n){
+  int size;
+  if (n>5000){
+    size = (int)(1.15 * n * log(n));
+  }
+  else{
+    size = (int)(1.3 * n * log(n) + 10);
+  }
+  printf("size: %d\n", size);
+  int * arr = (int *)  calloc(1, size); 
+  return arr;
+}
+
+int main(){
+  int * arr = generateArray(20);
+  return 0;
+}
